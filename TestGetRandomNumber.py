bd60bacc0ff00a92851a7600fd0b64a52012d88f
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-23 16:58:46 -0300

    feat: add if get_random_number returns a number in range

--- TestGetRandomNumber.py
+++ TestGetRandomNumber.py
@@ -1,11 +1,16 @@
 import unittest
 
-def get_random_number():
+def get_random_number(range):
     return 0
 
 class TestGetRandomNumber(unittest.TestCase):
     def test_if_return_a_number(self):
-        self.assertIsInstance(get_random_number(), int)
+        self.assertIsInstance(get_random_number(1), int)
+    
+    def test_it_return_a_number_in_range(self):
+        mock_random_range = 10
+        
+        self.assertTrue(get_random_number(mock_random_range) in list(range(10)))
 
 
 if __name__ == '__main__':
