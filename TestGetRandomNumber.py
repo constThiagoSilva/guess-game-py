0fc05dbd8e75f84f512e7e090f60c08b42e228fc
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-23 17:03:48 -0300

    refactor: add one more parameter to get_random_number function

--- TestGetRandomNumber.py
+++ TestGetRandomNumber.py
@@ -1,20 +1,21 @@
 import unittest
 from random import randint
 
-def get_random_number(range):
-    return randint(0, range)
+def get_random_number(minimal_range ,maximum_range):
+    return randint(minimal_range, maximum_range)
 
 class TestGetRandomNumber(unittest.TestCase):
     def test_if_return_a_number(self):
-        self.assertIsInstance(get_random_number(1), int)
+        self.assertIsInstance(get_random_number(1, 1), int)
     
     def test_it_return_a_number_in_range(self):
-        mock_random_range = 10
+        mock_minimum_range = 0
+        mock_maximum_range = 10
 
-        self.assertTrue(get_random_number(mock_random_range) in list(range(10)))
+        self.assertTrue(get_random_number(mock_minimum_range ,mock_maximum_range) in list(range(10)))
 
     def test_it_not_return_negative_number(self):
-        self.assertFalse(get_random_number(1) < 0)
+        self.assertFalse(get_random_number(1, 1) < 0)
 
 
 if __name__ == '__main__':
