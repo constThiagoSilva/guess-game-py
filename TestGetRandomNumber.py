5208222f8c978a962a022bf173c67d5d5fbf839e
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-23 17:01:27 -0300

    feat: add test if get_random_number not return a ngative number

--- TestGetRandomNumber.py
+++ TestGetRandomNumber.py
@@ -1,7 +1,8 @@
 import unittest
+from random import randint
 
 def get_random_number(range):
-    return 0
+    return randint(0, range)
 
 class TestGetRandomNumber(unittest.TestCase):
     def test_if_return_a_number(self):
@@ -9,8 +10,11 @@ class TestGetRandomNumber(unittest.TestCase):
     
     def test_it_return_a_number_in_range(self):
         mock_random_range = 10
-        
+
         self.assertTrue(get_random_number(mock_random_range) in list(range(10)))
+
+    def test_it_not_return_negative_number(self):
+        self.assertFalse(get_random_number(1) < 0)
 
 
 if __name__ == '__main__':
