c8c115c2506c3943674d2f226fab93d6f905653f
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-24 10:25:21 -0300

    refactor: clean the game

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 from getRandomNumber import get_random_number
 
-print('JOGO DO ADIVINHA NÚMEROS \n' + '-' * 60)
+print('JOGO DO ADIVINHA NÚMEROS \n' + '-' * 50)
 
 while True:
     try:
-        minimum_range = int(input('insira um número minimo para adivinhar:'))
-        maximum_range = int(input('insira um número máximo para adivinhar'))
+        minimum_range = int(input('Insira um número minimo para adivinhar: \n'))
+        print('-' * 50)
+        maximum_range = int(input('Insira um número máximo para adivinhar: \n'))
+        print('-' * 50)
         
         number_to_be_guessed = get_random_number(minimum_range, maximum_range)
         while True:
             while True:
                 try:
-                    chosen_number_by_user = int(input('insira o número que voce ache ser o certo:'))
+                    chosen_number_by_user = int(input('Insira o número que voce ache ser o certo: \n'))
+                    print('-' * 50)
 
                     if chosen_number_by_user > maximum_range or chosen_number_by_user < minimum_range:
                         raise
 
                     if number_to_be_guessed == chosen_number_by_user:
-                        print('Parabens! Voce ganhou')
-                        isExit = input('Voce quer continuar: s/n')
+                        print('Parabens! Voce ganhou.')
+                        isExit = input('Voce quer continuar: s/n \n')
 
                         if 'n' in isExit:
                             break
 
-                        minimum_range = int(input('insira um número minimo para adivinhar:'))
-                        maximum_range = int(input('insira um número máximo para adivinhar'))
+                        minimum_range = int(input('Insira um número minimo para adivinhar: \n'))
+                        print('-' * 50)
+                        
+                        maximum_range = int(input('Insira um número máximo para adivinhar: \n'))
+                        print('-' * 50)
         
                         number_to_be_guessed = get_random_number(minimum_range, maximum_range)
                     elif number_to_be_guessed > chosen_number_by_user:
-                        print('Tente um número mais alto!')
+                        print('Tente um número mais alto. \n')
                     else:
-                        print('Tenter um número mais baixo!')
+                        print('Tente um número mais baixo. \n')
                 except:
-                    print('insira um numero entre os numeros dados')
+                    print('Insira um número entre os números dados')
             break
         break
     except:
-        print('insira um número válido!!!!')
+        print('Insira um número válido!!!!')
