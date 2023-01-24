415edc04a2abd95ca706e6f378a40acff384fd44
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-24 10:13:42 -0300

    feat: add error if number given is minor or mayor than limits

--- main.py
+++ main.py
@@ -10,22 +10,31 @@ while True:
         number_to_be_guessed = get_random_number(minimum_range, maximum_range)
 
         while True:
-            chosen_number_by_user = int(input('insira o número que voce ache ser o certo:'))
+            while True:
+                try:
+                    chosen_number_by_user = int(input('insira o número que voce ache ser o certo:'))
 
-            print(chosen_number_by_user, number_to_be_guessed)
 
-            if number_to_be_guessed == chosen_number_by_user:
-                print('Parabens! Voce ganhou')
-                isExit = input('Voce quer continuar: s/n')
 
-                if 'n' in isExit:
-                    break
+                    if chosen_number_by_user > maximum_range or chosen_number_by_user < minimum_range:
+                        raise
 
-            elif number_to_be_guessed > chosen_number_by_user:
-                print('Tente um número mais alto!')
-            else:
-                print('Tenter um número mais baixo!')
+                    print(chosen_number_by_user, number_to_be_guessed)
 
+                    if number_to_be_guessed == chosen_number_by_user:
+                        print('Parabens! Voce ganhou')
+                        isExit = input('Voce quer continuar: s/n')
+
+                        if 'n' in isExit:
+                            break
+
+                    elif number_to_be_guessed > chosen_number_by_user:
+                        print('Tente um número mais alto!')
+                    else:
+                        print('Tenter um número mais baixo!')
+                except:
+                    print('insira um numero entre os numeros dados')
+            break
         break
     except:
         print('insira um número válido!!!!')
